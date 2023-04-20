d72a6647d62e4115e09419737cd1c44e59cbafae
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-20 14:31:52 -0700

    add file of task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*task 4: hello, puts
+ * a C program that prints exactly "Programming is like building a multilingual puzzle and retur 0
+ *using the function puts()
+ * */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
