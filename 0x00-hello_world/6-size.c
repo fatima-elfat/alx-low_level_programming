1d52e87097640fe9c51c95f0a1c0269284679fca
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-20 15:03:06 -0700

    add file of task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Main function
+ *Return: Value of 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (char)str.size());
+	printf("Size of a int: %d byte(s)\n", (int)str.size());
+	printf("Size of a long int: %d byte(s)\n", (long int)str.size());
+	printf("Size of a long long int: %d byte(s)\n", (long long int)str.size());
+	printf("Size of a float: %d byte(s)\n", (float)str.size());
+	return (0);
+}
