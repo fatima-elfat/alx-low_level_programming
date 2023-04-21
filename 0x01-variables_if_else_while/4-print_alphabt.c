df592510eeb9251e05a03513ae633931af70dc11
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-21 14:54:27 -0700

    add file of task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Write lowercase a-z and remove q and e
+ *Return: Return 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
