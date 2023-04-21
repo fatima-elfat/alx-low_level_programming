c46c35aa39aae2cbd8bd354536dc5a1f595416e5
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-21 15:39:33 -0700

    add file of task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no duplicate digits
+ *Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (b = '0'; b <= '9';b++)
+	{
+		for (a = (b + 1); a <= '9'; a++)
+		{
+			putchar(b);
+			putchar(a);
+
+			if (b != '8' || a != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
