64c8506832c3d2fa215c38ec10a1437f58fdb194
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-21 16:10:44 -0700

    edit file of task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - print a num pair from 00-99
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int aa;
+	int bb;
+
+for ( a = 0 ; a < 10 ; a++)
+{	
+
+	for (b = 0 ; b <= 10 ; b++)
+	{
+	
+		for (aa = 0 ; aa < 10 ; aa++)
+		{
+			for ( bb = 0 ; bb < 10 ; bb++)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				putchar(' ');
+				putchar(aa);
+				putchar(bb);
+				if (!((a == 9 && b == 8) && (aa == 9 && bb == 8)))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				
+			} bb = 0;
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}
 
