3ca485b41269c0ccfda5b9a37afcf1a24c1b8480
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 13:59:18 -0700

    add file of task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_last_digit - last digit of input number
+ * @i: number
+ * Return: the last digit
+ */
+int print_last_digit(int i)
+{
+	char l[];
+	int o;
+	int c;
+
+	l = i;
+	o = (int)strlen(l);
+	c = l[o];
+	_putchar((char)c);
+	return (c);
+}
