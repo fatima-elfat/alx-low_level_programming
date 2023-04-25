86750d9b400d35875d8327fe1dce4e689a413808
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 03:46:29 -0700

    add file of task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print "_putchar"
+ * Return: 0
+ */
+int main(void)
+{
+	char l[] = "_putchar";
+
+	for(i=0; i<strlen(l); i++)
+	{
+	       _putchar(l[i]);
+	}
+	_putchar("\n");
+	return (0);
+}		
