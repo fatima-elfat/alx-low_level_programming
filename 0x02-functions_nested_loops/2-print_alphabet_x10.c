1e138549158ad16e08f35a5a24c5386842f7651f
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 10:09:03 -0700

    add file of task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+		
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		       _putchar(c);
+		}
+	}
+	_putchar('\n');
+}
