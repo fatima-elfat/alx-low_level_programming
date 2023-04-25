3e9bdf1b2638fff00ccfe197a03cbef3a029b836
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 16:44:04 -0700

    add file of task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int i, j, o, a, b, c;
 
-	if ( n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -17,32 +17,32 @@ void print_times_table(int n)
 				o = i * j;
 				if (j == 0)
 					_putchar('0' + o);
-				else if (o < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + o);
-				}
-				else if (o < 100)
-				{
-					a = o % 10;
-					b = (o - a) / 10;
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + b);
-					_putchar('0' + a);
-				}
 				else
 				{
-					a = o % 100;
-					c = (o - a) / 100;
-					_putchar(' ');
-					_putchar('0' + c);
-					b = a % 10;
-					c = (a - b) / 10;
-					_putchar('0' + c);
-					_putchar('0' + b);
+				       	if (o < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + o);
+					}
+					else if (o < 100)
+					{
+						a = o % 10;
+						b = (o - a) / 10;
+						_putchar(' ');
+						_putchar('0' + b);
+						_putchar('0' + a);
+					}
+					else
+					{
+						a = o % 100;
+						c = (o - a) / 100;
+						_putchar('0' + c);
+						b = a % 10;
+						c = (a - b) / 10;
+						_putchar('0' + c);
+						_putchar('0' + b);
+					}
 				}
 				if (j != n)
 					_putchar(',');
