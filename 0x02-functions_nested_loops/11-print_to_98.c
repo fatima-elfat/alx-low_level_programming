ffe9228229c86e97b9e4ffc353b1846c333eb6a1
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 15:58:58 -0700

    add file of task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,11 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 		}
-		printf("98 \n");
+		printf("98\n");
 	}
 	else
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
                         printf("%d, ", i);
                 }
