4abd555ae6e3d520d74b3a8bb067cb3bf4b95d2e
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 04:26:22 -0700

    edit file of task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	char l[] = "_putchar";
 	int i;
+	int l_max;
 
-	for(i = 0; i < (int)strlen(l); i++)
+	l_max = (int) strlen(l)
+
+	for(i = 0; i < l_max; i++)
 	{
 	       _putchar(l[i]);
 	}
 	_putchar('\n');
 	return (0);
-}		
+}
