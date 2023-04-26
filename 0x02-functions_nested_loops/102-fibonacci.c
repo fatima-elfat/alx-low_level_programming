4fdf363b2fce7e50e7032c88bdfba04fe852d2bf
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 17:13:26 -0700

    add file of task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 
 	i = 1;
 	j = 2;
-	printf("%d, %d", i, j);
+	printf("%d, %d, ", i, j);
 	for (k = 3; k <= 50; k++)
 	{
 		l = i + j;
 		i = j;
 		j = l;
-		printf("%d",j);
+		printf("%d", j);
 		if (k == 50)
 			putchar('\n');
 		else
