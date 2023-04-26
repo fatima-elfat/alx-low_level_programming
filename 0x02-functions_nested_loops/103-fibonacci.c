0748072a3238650e71debbf098efd9276c8d046c
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-25 17:31:15 -0700

    add file of task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - sum even fibonacci
+  * Return: Nothing.
+  */
+int main(void)
+{
+	long int a, b, c, d, s;
+
+	a = 1;
+	b = 2;
+	s = 2;
+	for (c = 2; c < 50; c ++)
+	{
+		d = a + b;
+		a = b;
+		b = d;
+		if ( b % 2 ==0 && b <= 4000000)
+			s = s +b;
+
+	}
+	return (0);
+}
