802524b1ff13f25d0773254fa3b757f7d98254d2
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-27 16:23:48 -0700

    add file of task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	unsigned long n = 612852475143;
+	unsigned long i, j;
+	for (i = n - 1; i > 1; i--)
+	{
+		j = n % i;
+		if (j == 0)
+		{
+			n = i;
+		}
+	}
+	printf("%lu\n", num);
+	return (0);
+}	
