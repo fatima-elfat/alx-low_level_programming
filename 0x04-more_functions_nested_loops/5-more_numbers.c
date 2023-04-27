eb1c8b3415bd6683ffca2962c53b37d7b1deb248
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-27 13:55:36 -0700

    add file of task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	for (i = 48; i < 53; i++)
+		_putchar(i);
+	_putchar('\n');
+}
