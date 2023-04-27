1191e161618d7eb369dfec2371a441ec2703e98d
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-27 15:29:40 -0700

    add file of task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 	printf("1");
 	for (i = 2; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+                        printf(" FizzBuzz");
+		else if (i % 3 == 0)
 			printf(" Fizz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
 		else
 			printf(" %d", i);
 	}
