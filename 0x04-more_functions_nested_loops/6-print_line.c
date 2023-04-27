7f3dc2dc08ffff2c1e5b5c57b5b9e159b6580d6b
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-27 14:15:45 -0700

    add file of task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_line - print the character "_" n times to draw a line
+ * @n: number of characters to draw
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < n+1; i++)
+		_putchar('_');
+
+	_putchar('\n');
+}
