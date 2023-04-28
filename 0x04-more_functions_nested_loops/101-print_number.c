320c2d05ebb1f0d0a7a098b248e15c0418859759
fatima-elfat <ffzelfatehy@gmail.com>
2023-04-27 17:22:18 -0700

    add file of task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,34 +3,31 @@
  * print_number - print an int
  * @n: int to be printed
  */
-
 void print_number(int n)
 {
-	int j, k, l, m;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		n *= -1;
-	}
-
-	j = 1000;
-	m = n;
+	unsigned int tens, digit, positive = n;
+	double t_beg = 1;
 
 	if (n == 0)
-		_putchar(48);
-	while (j != 0)
+		_putchar('0');
+	else
 	{
-		if (n > j)
+		if (n < 0)
 		{
-			k = m % j;
-			l = (m - k) / j;
-			m = k;
-			_putchar(l + '0');
+			positive = n * -1;
+			_putchar('-');
 		}
-		if (j != 1)
-			j /= 10;
-		else
-			j = 0;
+
+		while (t_beg <= positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
 }
