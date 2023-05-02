d9447e9cb072165945227ae759e5324436705c71
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-02 14:52:21 -0700

    add file of task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	return (l);
+}
+/**
+ * _strcpy - copy the string
+ * @dest: destination of the string
+ * @src: souece of the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l, i;
+	l = _strlen(*src);
+
+	for(i = 0; i <= l; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
