12e7f06adca3e14c388bfd3b21b454087e61267f
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-02 13:56:14 -0700

    add file of task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	return (l);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char t;
+
+	while (i < j)
+	{
+		t = s[i];
+		s[i] = s[j];
+		s[j] = t;
+		i++;
+		j--;
+	}
+}
