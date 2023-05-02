efd7d62e5d23befbf2797a32c9741b1f92cd2d14
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-02 11:49:46 -0700

    add file of task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+
+	return (l);
+}
+/**
  * rev_string - reverses a string
  * @s: string to reverse
  */
@@ -8,13 +26,14 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int j = (_strlen(s) - 1);
-	char tmp;
+	char t;
 
 	while (i < j)
 	{
-		tmp = s[i];
+		t = s[i];
 		s[i] = s[j];
-		s[j] = tmp;
-		i++, j--;
+		s[j] = t;
+		i++;
+		j--;
 	}
 }
