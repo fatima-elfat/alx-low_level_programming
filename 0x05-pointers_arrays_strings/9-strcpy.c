d3c01c784351fe5f6282392f666f9873fac5d719
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-02 14:55:03 -0700

    add file of task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -21,13 +21,15 @@ int _strlen(char *s)
  * _strcpy - copy the string
  * @dest: destination of the string
  * @src: souece of the string
+ * Return: copied string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int l, i;
+
 	l = _strlen(src);
 
-	for(i = 0; i <= l; i++)
+	for (i = 0; i <= l; i++)
 	{
 		dest[i] = src[i];
 	}
