637708eef1dd96e361b66cd43a1a4820526c8038
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-02 17:59:19 -0700

    add file of task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ */
+int main(void)
+{
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+
+	srand(time(NULL));
+	for(int i = 0; i < 4; i++) 
+	{
+		printf("%c", list[rand() % (sizeof list - 1)]);
+	}
+	return (0);
+}
