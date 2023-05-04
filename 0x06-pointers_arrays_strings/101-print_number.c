7358656afcfb315e4d6d3d256d5a20a653613dcf
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-04 16:45:19 -0700

    add file of task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_number - print an int
+ * @n: int to be printed
+ */
+
+void print_number(int n)
+{
+	int j, k, l, m;
+
+	if (n == 0)
+		_putchar(48);
+	else
+	{
+		if (n < 0)
+		{
+			_putchar(45);
+			n *= -1;
+		}
+		j = 1000;
+		m = n;
+		while (j != 0)
+		{
+			if (n > j)
+			{
+				k = m % j;
+				l = (m - k) / j;
+				m = k;
+				_putchar(l + '0');
+			}
+			if (j != 1)
+				j /= 10;
+			else
+				j = 0;
+		}
+	}
+}
