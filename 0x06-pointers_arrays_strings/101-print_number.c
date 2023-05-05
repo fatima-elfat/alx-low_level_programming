4e0c78351b841e6eab1e8d8b783d22877c4db225
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-04 17:34:46 -0700

    add file of task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,35 +6,13 @@
 
 void print_number(int n)
 {
-	int j, k, l, m;
-	double o = 0;
-
-	if (n == 0)
-		_putchar(48);
-	else
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar(45);
-			n *= -1;
-		}
-		while (n >= o)
-			o *= 10;
-		j = o / 10;
-		m = n;
-		while (j != 0)
-		{
-			if (n > j)
-			{
-				k = m % j;
-				l = (m - k) / j;
-				m = k;
-				_putchar(l + '0');
-			}
-			if (j != 1)
-				j /= 10;
-			else
-				j = 0;
-		}
+		_putchar('-');
+		n *= -1;
 	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+
 }
