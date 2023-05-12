5ef82331cbf551e22b83e903d5e3ae4eeb645f85
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-12 05:07:45 -0700

    add file of task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry function prints its name, followed by a new line.
+ * @argc: nummber of command line.
+ * @argv: array of command line.
+ * Return: 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-1]);
+	return (0);
+}
