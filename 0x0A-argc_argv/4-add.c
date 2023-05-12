8ebaf82909672362e88ab44810d9b834a2257adc
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-12 08:12:59 -0700

    add file of task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry function prints the addition of positive numbers.
+ * @argc: nummber of command line.
+ * @argv: array of command line.
+ * Return: 0 for success and 1 for error.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int i, a, s = 0;
+
+		for (i = 1; i < argc; i++)
+		{
+			a = atoi(argv[i]);
+			if (a <= 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				s += a;
+		}
+		printf("%d\n", s);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
