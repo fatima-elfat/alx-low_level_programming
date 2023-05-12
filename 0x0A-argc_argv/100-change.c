9e2c488abe2265755bfc67bc5f15b8b88c3fa102
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-12 12:07:46 -0700

    add file of task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * change_m - change money using coins
+ * @m: input value of money.
+ * @r: change.
+ */
+void change_m(int m, int r)
+{
+	int c[5] = {25, 10, 5, 2, 1};
+	int *p = c;
+	int temp;
+
+	if (m >= *p)
+	{
+		if (m % *p == 0)
+		{
+			r += m / *p;
+			printf("%d\n", r);
+		}
+		else
+		{
+			temp = m % *p;
+			r += (m - temp) / *p;
+			p++;
+			change_m(temp, r);
+		}
+	}
+	else
+	{
+		p++;
+		change_m(m, r);
+	}
+}
+/**
+ * main - entry function prints the minimum number of coins to make change for
+ * an amount of money.We can use an unlimited number of coins of values 25, 10
+ * , 5, 2, and 1 cent.
+ * @argc: nummber of command line.
+ * @argv: array of command line.
+ * Return: 0 for success and 1 for error.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int a;
+
+		a = atoi(argv[1]);
+		if (a <= 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		change_m(a, 0);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 void print(char *a[], int l);
-
+void change_m(int m, int r);
 #endif
