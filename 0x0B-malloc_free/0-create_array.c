1e14730ab9cf2b05f629d773dc87b38abb63c122
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-15 06:51:08 -0700

    add file of task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,13 +15,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	p = malloc(size * sizeof(char) + 1);
+	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}
-	p[i] = '\0';
 	return (p);
 }
