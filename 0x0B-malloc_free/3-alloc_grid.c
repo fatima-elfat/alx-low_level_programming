d067fa6317a62347abc20c721c77d0c745823aba
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-16 07:57:34 -0700

    add file of task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(sizeof(int) * height);
+	p = malloc(sizeof(int *) * height);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
