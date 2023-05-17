e87c019c707288f521c2a07a42120ead316ab582
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-16 17:18:13 -0700

    add file of task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,23 +1,43 @@
-#include "main.h"
+i#include "main.h"
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * strtow - splits a string into words.
+ * wordcount - calculate number of words in a string.
  * @str: string.
- * Return: splited string into array of words.
+ * Return: number of words.
  */
-char **strtow(char *str)
+int wordcount(char *str)
 {
-	int i, j = 0, w = 0, b, c, *t;
-	char **p, pc = ' ';
+	int i, w = 0;
+	char pc = ' ';
 
 	if (str == NULL)
-		return (NULL);
+		return (0);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' && pc != ' ')
-			w++;
-		pc = str[i]; }
+		if (str[i] == ' ')
+		{
+			if (pc != ' ')
+			{
+				w++;
+			}
+		}
+		pc = str[i];
+	}
+	return (w);
+}
+/**
+ * wordbe - list of word begining and ends of words in a string.
+ * @str: string.
+ * @w: number of string.
+ * Return: list of begin and ends.
+ */
+int *worbe(char *str, int w)
+{
+	int i, j = 0, *t;
+	char pc = ' ';
+
 	t = malloc(sizeof(int) * w * 2);
 	if (t == NULL)
 		return (NULL);
@@ -28,23 +48,63 @@ char **strtow(char *str)
 			if (pc != ' ')
 			{
 				t[j] = i - 1;
-				j++; }}
+				j++;
+			}
+		}
 		else if ((str[i] != '\0' || str[i] != ' ') && pc == ' ')
 		{
 			t[j] = i;
-			j++; }
-		pc = str[i]; }
+			j++;
+		}
+		pc = str[i];
+	}
+	return (t);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ * Return: splited string into array of words.
+ */
+char **strtow(char *str)
+{
+	int i, j, w, a, b, c, d, *t;
+	char **p, pc = ' ';
+
+	if (str == NULL)
+		return (NULL);
+	w = wordcount(str);
+	if (w == 0)
+	{
+		p = malloc(1);
+		if (p == NULL)
+			return (NULL);
+		p[0] = malloc(1);
+		if (p[0] == NULL)
+		{
+			free(p);
+			return (NULL); }
+		p[0][0] = '\0';
+		for (i = 0; p[i] != NULL; ++i)
+		{
+			p[i][0] = '\0'; }
+		return (p); }
+	t = worbe(str, w);
+	j = w * 2;
 	p = malloc(sizeof(char) * (j / 2));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < ((w * 2) - 1); i += 2)
+	d = (w * 2) - 1;
+	for (i = 0; i < d; i += 2)
 	{
+		a = t[i];
 		b = t[i + 1];
 		c = (i + 2) / 2;
-		p[c - 1] = malloc(sizeof(char) * (b - t[i] + 2));
+		p[c - 1] = malloc(sizeof(char) * (b - a + 2));
 		if (p[c - 1] == NULL)
 			return (NULL);
-		for (j = t[i]; j <= b; j++)
-			p[c - 1][j - t[i]] = str[j]; }
+		for (j = a; j <= b; j++)
+		{
+			p[c - 1][j - a] = str[j]; }
+		p[c - 1][j - a] = '\0'; }
 	free(t);
 	return (p); }
