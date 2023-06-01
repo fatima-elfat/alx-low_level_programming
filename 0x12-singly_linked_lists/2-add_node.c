ac5c771dcb7f4e8d71685e285d606f0e727bf040
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-01 11:09:04 -0700

    add file of mandatory tasks

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+include "lists.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: input string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0' || *s == '\n' || *s == 0)
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
+/**
+  * add_node - adds a new node at the beginning of a list_t list.
+  * @head: the linked list.
+  * @str: the string to add.
+  * Return: the address of the new list.
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *l;
+
+	if (head != NULL && str != NULL)
+	{
+		l = malloc(sizeof(list_t));
+		if (l == NULL)
+			return (NULL);
+		l->str = strdup(str);
+		l->len = _strlen_recursion(str);
+		l->next = *head;
+		*head = l;
+	}
+
+	return (l);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: input string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0' || *s == '\n' || *s == 0)
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
+
+/**
+  * add_node_end - adds a new node at the end of a list_t list.
+  * @head: the linked list.
+  * @str: the string to add.
+  * Return: the address of the new list.
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *l1, *l2;
+
+	if (str != NULL)
+	{
+		l1 = malloc(sizeof(list_t));
+		if (l1 == NULL)
+			return (NULL);
+		l1->str = strdup(str);
+		l1->len = _strlen_recursion(str);
+		l1->next = NULL;
+		if (*head != NULL)
+		{
+			l2 = *head;
+			while (l2->next != NULL)
+			{
+				l2 = l2->next;
+			}
+			l2->next = l1;
+			return (l2);
+		}
+		else
+		{
+			*head =  l1;
+			return (l1);
+		}
+	}
+}
