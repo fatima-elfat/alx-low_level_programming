15ef385eb97c1406611b683b09de83091279bdf2
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-01 11:36:55 -0700

    fix error in task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,26 @@
 #include "lists.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string followed by a new line
+ * @s: string to print.
+ * Return: length of string.
+ */
+int _puts(char *s)
+{
+	while (*s != '\0')
+		_putchar(*s++);
+
+	_putchar('\n');
+	return (_strlen(s));
+}
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -12,10 +34,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			_puts("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
+		{
+			_putchar("[");
+		}
 		h = h->next;
 		r++;
 	}
