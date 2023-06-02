ef971ccae3f017a1768b301997ebf549b5b4c956
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-02 04:42:23 -0700

    fix files of advanced tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,15 @@
 // Assembly printf hello, Holberton
 section .text
+extern printf
 global main
 
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, a
-	mov rdx, alen
-	syscall
+	mov eax, 1
+	mov edi, 1
+	mov esi, a
+	mov edx, alen
+	call printf
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
-
-section .rodata
+section .data
 	a: db "Hello, Holberton", 10
 	alen: equ $ - a
