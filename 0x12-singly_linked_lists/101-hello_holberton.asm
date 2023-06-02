a48569591ffa44ebb7d0f09446f93b943a1730e5
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-02 04:34:36 -0700

    fix files of advanced tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,13 @@
 // Assembly printf hello, Holberton
-hello: .string "Hello, Holberton\n"
+section .text
+extern printf
+global main
 
-      .balign 4                   // must be word aligned
-      .global main
-main: stp  x29, x30, [sp, -20]!   // allocating 20 bytes
-      mov  x29, sp                // Update FP to current SP
+main:
+	mov edi, msg
+	mov eax, 0
+	call printf
 
-      adrp x0, hello              // set 1st argument of printf(format, var1, var2...)
-      add  x0, x0, :lo12:hello    // set 1st argument to pass to printf
-      bl   printf                 // Call printf()
-
-      // Set up return value of main()
-      mov  w0, 0
-
-      ldp  x29, x30, [sp], 20
-      ret                  
+section .data
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg 
