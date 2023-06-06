3d772b8c0f56f72359023d6a4aedc5004fd1e433
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-06 16:28:31 -0700

    fix errors in file of mandatory tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (temp->next == NULL)
 		{
+			free(node);
 			return (NULL);
 		}
 		i++;
@@ -40,6 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (i != idx)
 	{
+		free(node);
 		return (NULL);
 	}
 	node->next = temp->next;
