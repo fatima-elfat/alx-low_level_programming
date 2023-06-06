53a9e6e0ceb20bb945788b7e792482c5f0eb933b
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-06 14:00:49 -0700

    add file of advanced tasks 2

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+/**
+ * isloop - detects loops using Floyd’s Cycle-Finding Algorithm.
+ * @head: the input struct.
+ * @num: the number of nodes.
+ * Return: 1 for true.
+ */
+int isloop(const listint_t *head, int *num)
+{
+	int r = 0;
+	const listint_t *a = head, *b = head;
+
+	*num = 0;
+	while (a && b && b->next)
+	{
+		a = a->next;
+		b = b->next->next;
+		*num += 1;
+		if (b == a)
+		{
+			r = 1;
+			a = head;
+			while (a != b)
+			{
+				a = a->next;
+				b = b->next;
+				*num += 1;
+			}
+		}
+	}
+	return (r);
+}
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: the input struct.
+ * Return: the number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int i = 0, j, k, num;
+	const listint_t *temp;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	j = isloop(head, &num);
+	if (j == 0)
+	{
+		temp = head;
+		while (temp)
+		{
+			printf("[%p] %d\n", (void *)temp, temp->n);
+			i++;
+			temp = temp->next;
+		}
+	}
+	else
+	{
+		for (k = 0; k < num; k++)
+		{
+			printf("[%p] %d\n", (void *)temp, temp->n);
+			i++;
+			temp = temp->next;
+		}
+	}
+	return (i);
+}
