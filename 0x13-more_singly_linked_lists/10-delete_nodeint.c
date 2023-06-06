04c383a9bc51aaa9d9c9f4631e0bce8fbf3319a7
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-06 13:04:16 -0700

    fix errors in file of mandatory tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp1);
 		return (1);
 	}
-	while (i != (index - 1) || temp1->next != NULL)
+	while (i < (index - 1) || temp1->next != NULL)
 	{
 		i++;
 		temp1 = temp1->next;
