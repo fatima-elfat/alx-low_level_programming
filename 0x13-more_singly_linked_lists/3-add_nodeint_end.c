306207ee412ce0df642ad2f12dc23c3518e83e42
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-06 12:49:13 -0700

    fix errors in file of mandatory tasks 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,6 +9,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
 	{
