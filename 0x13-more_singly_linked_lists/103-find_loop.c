df01cb64a8f2a1ea7a2083eaf1e4bfa8daa6fdf0
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-06 18:01:34 -0700

    add file of advanced tasks 4

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: the input struct.
+ * Return: The address of the node where the loop starts
+ * , or NULL if there is no loop.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+	listint_t *a, *b;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	a = head;
+	b = a;
+	while (a && b)
+	{
+		a = a->next;
+		b = b->next->next;
+
+		if (a == b)
+		{
+			a = b;
+			while (a != b)
+			{
+				a = a->next;
+				b = b->next;
+			}
+			return (a);
+		}
+	}
+	return (NULL);
+
+}
