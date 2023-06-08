9f10485526a9c3444d68365be0fcf4b9a9968c0c
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-08 23:51:05 +0100

    fix erroor in file of tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,24 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int a;
-
-	a = n;
 	if (n == 0)
 	{
-		_putchar('0');
-		return;
+		putchar('0');
 	}
-	while (a > 0)
+	if (n >> 1)
 	{
-		if ((a & n) == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-		a = (a >> 1);
+		print_binary(n >> 1);
+	}
+	
+	if ((1 & n) == 0)
+	{
+		putchar('0');
+	}
+	else
+	{
+	_putchar('1');
 	}
 }
