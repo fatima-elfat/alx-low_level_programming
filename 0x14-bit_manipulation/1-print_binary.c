88afe0a0c0103d2d6f8bb5f3613ce26e10ec5e07
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-08 16:03:57 -0700

    fix file of mandatory task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,20 +12,16 @@ void print_binary(unsigned long int n)
 		putchar('0');
 		return;
 	}
-	if (n >> 1)
+	if (n >> 0 && n >> 1)
 	{
 		print_binary(n >> 1);
-		if ((1 & n) == 0)
-		{
-			putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
+	}
+	if ((1 & n) == 0)
+	{
+		putchar('0');
 	}
 	else
 	{
-		putchar('0');
+		_putchar('1');
 	}
 }
