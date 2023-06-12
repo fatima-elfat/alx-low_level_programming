e7e1b05bf9f512ce7ea1e2d78f4f7ffc552b9b26
fatima-elfat <ffzelfatehy@gmail.com>
2023-06-12 12:37:57 -0700

    fix erroor in file of tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		r = write(a, text_content, len);
 		if (r != len || r == -1)
