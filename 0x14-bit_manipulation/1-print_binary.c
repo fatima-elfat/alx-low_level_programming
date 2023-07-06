f4d812fe8873e32d7366b72f0a60fb28cda6e968
fatima-elfat <ffzelfatehy@gmail.com>
2023-07-06 07:20:56 -0700

    add file of task 1.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @n: unsigned int
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	a = n;
+	while (a > 0)
+	{
+		if ((a & n) == 0)
+		{
+			_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+		}
+		a = (a >> 1);
+
+	}
+}
+
