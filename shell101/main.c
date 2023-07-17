275191aaa962907e593535365637d7fdf7e88d29
fatima-elfat <ffzelfatehy@gmail.com>
2023-07-17 10:21:26 -0700

    add file main and shell

---
+++ shell101/main.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+
+/**
+ * main - main entry function.
+ * @argc: the number of arguments.
+ * @argv: the list of arguments.
+ * @env: the variable ennvironment.
+ * Return: 0 on success.
+ */
+int main(int argc, char **argv, char **env)
+{
+	l_u *e;
+
+	/* (void) argv;*/
+	e = _getenv(env);
+	/* a = get_aliases(); */
+	if (argc == 1)
+	{
+		i_mode(e);
+	}
+	/*
+	 * else if (argc > 1){
+	 * n_i_mode(argv, e); }
+	 */
+	return (0);
+}
---
+++ shell101/shell.h
@@ -0,0 +1,19 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+#define CUSTOM_GETLINE 0
+
+#endif
