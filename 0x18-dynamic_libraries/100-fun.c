8c4d7b5916f625001d3e46c76a69c4780c864c4e
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-21 11:32:54 -0700

    c

---
+++ 0x18-dynamic_libraries/100-fun.c
@@ -0,0 +1,52 @@
+/**
+ * add - adds.
+ * @a: int.
+ * @b: int.
+ * Return: ...
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts.
+ * @a: int.
+ * @b: int.
+ * Return: ...
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies.
+ * @a: int.
+ * @b: int.
+ * Return: ...
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides.
+ * @a: int.
+ * @b: int.
+ * Return: ...
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - calculate modul.
+ * @a: int.
+ * @b: int.
+ * Return: ...
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
