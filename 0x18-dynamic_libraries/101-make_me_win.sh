c57af13e43b9bf3a09649baa8fc3b91930ce2ed8
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-21 13:44:20 -0700

    d

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /h https://github.com/fatima-elfat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101.so
-export LD_PRELOAD=/h/lib101.so
+wget -P /temp https://github.com/fatima-elfat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101.so
+export LD_PRELOAD=/temp/lib101.so
