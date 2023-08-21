f6a31bb803c1eb254265fd84f8040dd5ee854d56
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-21 15:15:39 -0700

    fix errors

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /rep_temp https://github.com/fatima-elfat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101.so
-export LD_PRELOAD=/rep_temp/lib101.so
+wget -P /tmp https://github.com/fatima-elfat/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
+export LD_PRELOAD=/tmp/librand.so
