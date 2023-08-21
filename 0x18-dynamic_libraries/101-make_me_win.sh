00961ecb9be33d4171bf25917f5a1e1f26c06450
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-21 14:58:04 -0700

    try example

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /rep_temp https://github.com/fatima-elfat/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101.so
-export LD_PRELOAD=/rep_temp/lib101.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
